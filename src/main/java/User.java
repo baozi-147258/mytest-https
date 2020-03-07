54c230e46e2d9593411216fdcf925ddf9d1f5d5d
jyl <111@qq.com>
2020-03-08 01:46:12 +0800

    2

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -9,7 +9,7 @@ import java.util.Date;
  **/
 public class User {
     private String id;
-    private String name;
+    private String usernames;
     private Date birthday;
     private String sex;
     private String address;
